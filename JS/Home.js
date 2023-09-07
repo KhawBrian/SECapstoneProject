17c7d1c3a01e622a3461c69ea1ff7c2b7a6d052b
ImamGozali <imamgozali.b@gmail.com>
2023-09-07 21:35:34 +0700

    update html, css, javascript

--- JS/Home.js
+++ JS/Home.js
@@ -1,6 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("#show-login").addEventListener("click",function(){document.querySelector(".popup").classList.add("active");
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("#show-login").addEventListener("click", function () {
+        document.querySelector(".popup").classList.add("active");
+    });
+    document.querySelector(".popup .close-btn").addEventListener("click", function () {
+        document.querySelector(".popup").classList.remove("active");
+    });
 });
-document.querySelector(".popup .close-btn").addEventListener("click",function(){document.querySelector(".popup").classList.remove("active");
-});    
-}); 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll(".img-slide");
+    const navButtons = document.querySelectorAll(".nav-btn");
+    let currentSlide = 0;
+
+    // Fungsi untuk menampilkan slide tertentu
+    function showSlide(slideIndex) {
+        slides.forEach((slide, index) => {
+            if (index === slideIndex) {
+                slide.classList.add("active");
+                navButtons[index].classList.add("active");
+            } else {
+                slide.classList.remove("active");
+                navButtons[index].classList.remove("active");
+            }
+        });
+    }
+
+    // Fungsi untuk pindah ke slide berikutnya
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    // Fungsi untuk memulai otomatisasi
+    function startAutoSlide() {
+        setInterval(nextSlide, 8000); // Ganti slide setiap 8 detik
+    }
+
+    // Event listener untuk tombol navigasi
+    navButtons.forEach((button, index) => {
+        button.addEventListener("click", () => {
+            showSlide(index);
+            currentSlide = index;
+        });
+    });
+
+    // Memulai otomatisasi slide
+    startAutoSlide();
+});
